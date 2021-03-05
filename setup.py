f1aabefba7f850f34993a7a7755d18cc56c5195b
Douglas Blank <doug.blank@gmail.com>
2021-03-05 15:47:15 -0800

    Bring versions up to date

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-# **************************************************
+# *******************************************************
 # aitk: Python tools for AI
 #
-# Copyright (c) 2021 ArtificialIntelligenceToolkit
+# Copyright (c) 2021 AITK Developers
 #
 # https://github.com/ArtificialIntelligenceToolkit/aitk/
 #
-# **************************************************
+# *******************************************************
 
 """
 aitk setup
@@ -16,28 +16,30 @@ import os
 
 import setuptools
 
-HERE = os.path.abspath(os.path.dirname(__file__))
-
-# The name of the project
 name = "aitk"
-
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup_args = dict(
     name=name,
-    version="1.0.0",
+    version="1.0.1",
     url="https://github.com/ArtificialIntelligenceToolkit/%s" % name,
     author="Douglas Blank",
     description="Python tools for AI",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    install_requires=["aitk.robots", "aitk.networks", "aitk.utils"],
+    install_requires=[
+        "aitk.robots>=0.7.4",
+        "aitk.networks>=0.2.4",
+        "aitk.utils>=0.0.4"
+    ],
     python_requires=">=3.6",
     license="BSD-3-Clause",
     platforms="Linux, Mac OS X, Windows",
-    keywords=["robot", "simulator", "jupyter", "python"],
+    keywords=["ai", "artificial intelligence", "robots",
+              "simulator", "jupyter", "python", "machine learning",
+              "neural networks", "keras", "tensorflow"],
     classifiers=[
         "License :: OSI Approved :: BSD License",
         "Programming Language :: Python",
@@ -45,6 +47,7 @@ setup_args = dict(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Framework :: Jupyter",
     ],
 )
