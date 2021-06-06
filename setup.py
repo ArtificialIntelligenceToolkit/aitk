32e15be16f4c919a7c1822de473db3bee4c499aa
Doug Blank <doug.blank@gmail.com>
2021-06-06 08:12:44 -0700

    Version 1.0.4

--- setup.py
+++ setup.py
@@ -25,14 +25,14 @@ with open(os.path.join(HERE, "README.md"), "r") as fh:
 
 setup_args = dict(
     name=name,
-    version="1.0.3",
+    version="1.0.4",
     url="https://github.com/ArtificialIntelligenceToolkit/%s" % name,
     author="Douglas Blank",
     description="Python tools for AI",
     long_description=long_description,
     long_description_content_type="text/markdown",
     install_requires=[
-        "aitk.algorithms>=0.0.2",
+        "aitk.algorithms>=0.0.3",
         "aitk.robots>=0.9.24",
         "aitk.networks>=0.3.10",
         "aitk.utils>=0.4.3"
