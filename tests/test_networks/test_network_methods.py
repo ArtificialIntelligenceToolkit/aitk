037dbf89d03ae8e94832b6a068648035a65788bf
Lisa Meeden <meeden@cs.swarthmore.edu>
2024-09-24 16:05:53 -0400

    added more tests

--- tests/test_networks/test_network_methods.py
+++ tests/test_networks/test_network_methods.py
@@ -13,31 +13,22 @@ from tensorflow.keras.layers import Dense, InputLayer
 
 from aitk.networks import Network, SimpleNetwork
 
-"""
-Network class methods that return a value
--fit: history
--compile: result of model.compile
--predict: numpy array of output activations
--get_input_length: int representing size of input layer
--predict_to: numpy array of layer activations
--predict_from: numpy array of layer activations
--propagate_to: numpy array of layer activations
-
-SimpleNetwork class methods
--make_layer: Dense layer
-"""
-
 
 def test_set_weights():
     network = SimpleNetwork(3, 2, 1)
     network.set_weights([1, 1, 1, 1, 1, 1, -2.5, -1.5, -3, 2, 0])
     inputs = [[0, 0, 0], [1, 0, 0], [1, 1, 0], [1, 1, 1]]
-    expected_outputs = [[0.53426534], [0.5517651], [0.5280447], [0.44220227]]
+    expected_outputs = [
+        [0.53426534],
+        [0.5517651],
+        [0.5280447],
+        [0.44220227]
+    ]
     for i in range(len(inputs)):
         output = network.propagate(inputs[i])
         assert np.allclose(output, expected_outputs[i])
 
-
+        
 def test_propagate_to():
     network = SimpleNetwork(3, 2, 1)
     network.set_weights([1, 1, 1, 1, 1, 1, -2.5, -1.5, -3, 2, 0])
@@ -52,7 +43,22 @@ def test_propagate_to():
         actual_activations = list(network.propagate_to(inputs[i], "hidden"))
         assert np.allclose(actual_activations, expected_activations[i])
 
+        
+def test_predict():
+    network = SimpleNetwork(3, 2, 1)
+    network.set_weights([1, 1, 1, 1, 1, 1, -2.5, -1.5, -3, 2, 0])
+    inputs = [[0, 0, 0], [1, 0, 0], [1, 1, 0], [1, 1, 1]]
+    expected_activations = [
+        0.53426534,
+        0.5517651,
+        0.5280447,
+        0.44220227
+    ]
+    results = network.predict(np.array(inputs))
+    actual_activations = list(np.array(results).flatten())
+    assert np.allclose(actual_activations, expected_activations)
 
+    
 def test_predict_to():
     network = SimpleNetwork(3, 2, 1)
     network.set_weights([1, 1, 1, 1, 1, 1, -2.5, -1.5, -3, 2, 0])
@@ -67,6 +73,26 @@ def test_predict_to():
     for i in range(len(result)):
         actual_activations = list(result[i])
         assert np.allclose(list(result[i]), expected_activations[i])
+
+        
+def test_predict_from():
+    network = SimpleNetwork(3, 2, 1)
+    network.set_weights([1, 1, 1, 1, 1, 1, -2.5, -1.5, -3, 2, 0])
+    expected_activations = [
+        0.53426534,
+        0.5517651,
+        0.5280447,
+        0.44220227
+    ]
+    hiddens = [
+      [0.07585818, 0.18242551],
+      [0.18242551, 0.37754068],
+      [0.37754068, 0.62245935],
+      [0.62245935, 0.8175745 ]
+    ]
+    results = network.predict_from(np.array(hiddens), "hidden", "output")
+    actual_activations = list(np.array(results).flatten())
+    assert np.allclose(actual_activations, expected_activations)
 
 
 def test_train_from_set_weights():
