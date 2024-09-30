236f9c29078a65285f77e9bd747daf1f481d7d74
Lisa Meeden <meeden@cs.swarthmore.edu>
2024-09-30 16:51:45 -0400

    added tests for get_weights and topological_sort

--- tests/test_networks/test_network_methods.py
+++ tests/test_networks/test_network_methods.py
@@ -8,6 +8,21 @@
 #
 # ******************************************************
 
+
+"""
+Questions:
+
+In the file test_network.py, all test cases call connect
+and compile before using the networks. However, the 
+constructors for both the Network and SimpleNetwork classes
+call self.compile. And I would argue that the constructors
+should also call self.connect by default. When would there ever
+be a case when you wouldn't want the layers to be connected?
+
+
+"""
+
+
 import numpy as np
 from tensorflow.keras.layers import Dense, InputLayer
 
@@ -28,6 +43,20 @@ def test_set_weights():
         output = network.propagate(inputs[i])
         assert np.allclose(output, expected_outputs[i])
 
+def test_get_weights():
+    network = SimpleNetwork(3, 2, 1)
+    network.set_weights([1, 1, 1, 1, 1, 1, -2.5, -1.5, -3, 2, 0])
+    # weights are returned in this order:
+    # input->hidden
+    # hidden->output
+    # hidden biases
+    # output biases
+    weights = network.get_weights()
+    assert len(weights[0]) == 3
+    assert len(weights[1]) == 2
+    assert len(weights[2]) == 2
+    assert len(weights[3]) == 1
+    
         
 def test_propagate_to():
     network = SimpleNetwork(3, 2, 1)
@@ -137,3 +166,23 @@ def test_train_from_set_weights():
     for array in weights:
         actual_weights += list(array.flatten())
     assert np.allclose(expected_weights, actual_weights)
+
+    
+def test_topological_sort():
+    network = Network()
+    network.add(InputLayer([2], name="inputA"))
+    network.add(InputLayer([3], name="inputB"))
+    network.add(Dense(2, name="hiddenA"))
+    network.add(Dense(3, name="hiddenB"))
+    network.add(Dense(1, name="output"))
+    network.connect("inputA", "hiddenA")
+    network.connect("inputB", "hiddenB")
+    network.connect("hiddenA","output")
+    network.connect("hiddenB","output")
+    result = network.topological_sort(network._layers,
+                                  network._get_input_layers())
+    names = [layer.name for layer in result]
+    assert names[0][:-1] == names[1][:-1] == "input"
+    assert names[2][:-1] == names[3][:-1] == "hidden"
+    assert names[4] == "output"
+    
