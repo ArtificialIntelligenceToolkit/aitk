70ebbac3a6391b7b5cded65350757cf4d2dd8ac2
Lisa Meeden <meeden@cs.swarthmore.edu>
2024-10-01 16:54:00 -0400

    continue to add more tests

--- tests/test_networks/test_network_methods.py
+++ tests/test_networks/test_network_methods.py
@@ -9,24 +9,24 @@
 # ******************************************************
 
 
-"""
-Questions:
+"""Questions:
 
 In the file test_network.py, all test cases call connect
 and compile before using the networks. However, the 
 constructors for both the Network and SimpleNetwork classes
-call self.compile. And I would argue that the constructors
-should also call self.connect by default. When would there ever
-be a case when you wouldn't want the layers to be connected?
+call self.compile. So is the compile call necessary?
 
+Should the constructors also call self.connect by default. When would
+there ever be a case when you wouldn't want the layers to be
+connected?
 
 """
 
 
 import numpy as np
 from tensorflow.keras.layers import Dense, InputLayer
-
 from aitk.networks import Network, SimpleNetwork
+from aitk.utils import get_dataset
 
 
 def test_set_weights():
@@ -124,6 +124,21 @@ def test_predict_from():
     assert np.allclose(actual_activations, expected_activations)
 
 
+def test_propagate():
+    network = SimpleNetwork(3, 2, 1)
+    network.set_weights([1, 1, 1, 1, 1, 1, -2.5, -1.5, -3, 2, 0])
+    inputs = [[0, 0, 0], [1, 0, 0], [1, 1, 0], [1, 1, 1]]
+    expected_activations = [
+        [0.53426534],
+        [0.5517651],
+        [0.5280447],
+        [0.44220227]
+    ]
+    for i in range(len(inputs)):
+        result = network.propagate(np.array(inputs[i]))
+        assert np.allclose(result, expected_activations[i])
+
+        
 def test_train_from_set_weights():
     network = SimpleNetwork(3, 2, 1)
     network.set_weights([1, 1, 1, 1, 1, 1, -2.5, -1.5, -3, 2, 0])
@@ -169,6 +184,11 @@ def test_train_from_set_weights():
 
     
 def test_topological_sort():
+    #     output
+    #    /      \
+    # hiddenA  hiddenB
+    #    |        |
+    # inputA   inputB
     network = Network()
     network.add(InputLayer([2], name="inputA"))
     network.add(InputLayer([3], name="inputB"))
@@ -179,10 +199,89 @@ def test_topological_sort():
     network.connect("inputB", "hiddenB")
     network.connect("hiddenA","output")
     network.connect("hiddenB","output")
+    network.compile()
     result = network.topological_sort(network._layers,
                                   network._get_input_layers())
     names = [layer.name for layer in result]
     assert names[0][:-1] == names[1][:-1] == "input"
     assert names[2][:-1] == names[3][:-1] == "hidden"
     assert names[4] == "output"
+
+def test_get_input_from_dataset():
+    network = SimpleNetwork(
+        (6,6),
+        "Flatten",
+        10,
+        (10, "softmax"))
+    test_inputs, test_targets = get_dataset("validate_6x6")
+    result = network.get_input_from_dataset(0, test_inputs)
+    diff = result - test_inputs[0]
+    assert np.count_nonzero(diff) == 0
+
+
+def test_get_target_from_dataset():
+    network = SimpleNetwork(
+        (6,6),
+        "Flatten",
+        10,
+        (10, "softmax"))
+    test_inputs, test_targets = get_dataset("validate_6x6")
+    result = network.get_target_from_dataset(0, test_targets)
+    diff = result - test_targets[0]
+    assert np.count_nonzero(diff) == 0
+
     
+def test_get_input_from_banked_dataset():
+    # outputA outputB
+    #     \    /
+    #     hidden
+    #     /    \
+    # inputA  inputB
+    network = Network()
+    network.add(InputLayer([2], name="inputA"))
+    network.add(InputLayer([3], name="inputB"))
+    network.add(Dense(4, name="hidden"))
+    network.add(Dense(1, name="outputA"))
+    network.add(Dense(2, name="outputB"))
+    network.connect("inputA", "hidden")
+    network.connect("inputB", "hidden")
+    network.connect("hidden","outputA")
+    network.connect("hidden","outputB")
+    network.compile()
+    inputs = [np.array([[0,0],[1,0],[1,1]]),
+              np.array([[0,0,0],[1,0,1],[1,1,1]])]
+    targets = [np.array([[0],[0],[1]]),
+               np.array([[0,0],[1,0],[1,1]])]
+    result = network.get_input_from_dataset(2, inputs)
+    diff = inputs[0][2] - result[0]
+    assert np.count_nonzero(diff) == 0
+    diff = inputs[1][2] - result[1]
+    assert np.count_nonzero(diff) == 0
+
+
+def test_get_target_from_banked_dataset():
+    # outputA outputB
+    #     \    /
+    #     hidden
+    #     /    \
+    # inputA  inputB
+    network = Network()
+    network.add(InputLayer([2], name="inputA"))
+    network.add(InputLayer([3], name="inputB"))
+    network.add(Dense(4, name="hidden"))
+    network.add(Dense(1, name="outputA"))
+    network.add(Dense(2, name="outputB"))
+    network.connect("inputA", "hidden")
+    network.connect("inputB", "hidden")
+    network.connect("hidden","outputA")
+    network.connect("hidden","outputB")
+    network.compile()
+    inputs = [np.array([[0,0],[1,0],[1,1]]),
+              np.array([[0,0,0],[1,0,1],[1,1,1]])]
+    targets = [np.array([[0],[0],[1]]),
+               np.array([[0,0],[1,0],[1,1]])]
+    result = network.get_target_from_dataset(2, inputs)
+    diff = inputs[0][2] - result[0]
+    assert np.count_nonzero(diff) == 0
+    diff = inputs[1][2] - result[1]
+    assert np.count_nonzero(diff) == 0
